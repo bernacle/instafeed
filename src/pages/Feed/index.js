315d11b06a08ffbff399fa0fe91f3198e54bd4eb
Bruno  Simplício <brunosimplicio@MacBook-Pro-de-Bruno.local>
2020-04-05 18:03:04 -0300

    onRefresh functionality

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -17,8 +17,9 @@ export default function Feed() {
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
-  async function loadPage(pageNumber = page) {
+  async function loadPage(pageNumber = page, shouldRefresh = false) {
     if (total && pageNumber > total) return;
 
     setLoading(true);
@@ -31,7 +32,7 @@ export default function Feed() {
     const totalItems = response.headers.get('x-total-count');
 
     setTotal(Math.floor(totalItems / 5));
-    setFeed([...feed, ...data]);
+    setFeed(shouldRefresh ? data : [...feed, ...data]);
     setPage(pageNumber + 1);
     setLoading(false);
   }
@@ -40,6 +41,14 @@ export default function Feed() {
     loadPage();
   }, []);
 
+  async function refreshList() {
+    setRefreshing(true);
+
+    await loadPage(1, true);
+
+    setRefreshing(false);
+  }
+
   return (
     <View>
       <FlatList
@@ -47,6 +56,8 @@ export default function Feed() {
         keyExtractor={(post) => String(post.id)}
         onEndReached={() => loadPage()}
         onEndReachedThreshold={0.1}
+        onRefresh={refreshList}
+        refreshing={refreshing}
         ListFooterComponent={loading && <Loading />}
         renderItem={({item}) => (
           <Post>
